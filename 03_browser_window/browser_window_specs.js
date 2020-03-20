9c8409fd3378c7c5338ebe0e5bdd7b91c894831c
Scott D'Alessandro <scott@fullstackacademy.com>
2020-03-19 23:07:46 -0400

    Update browser_window_specs.js

--- 03_browser_window/browser_window_specs.js
+++ 03_browser_window/browser_window_specs.js
@@ -17,13 +17,13 @@ describe('BrowserWindow', () => {
     expect(firefoxWindow.tabs).toEqual([{ tabURL: 'twitter.com' }]);
   });
 
-  it('the BrowserWindow function sets adds a default tab if the tabs argument is omitted', () => {
+  it('the BrowserWindow function adds a default tab if the tabs argument is omitted', () => {
     expect(chromeWindow.accountName).toBe('Robert Brown');
     expect(chromeWindow.windowName).toBe('Email');
     expect(chromeWindow.tabs).toEqual([{ tabURL: 'defaultHomePage.com' }]);
   });
 
-  it('has a joinWindows method on its prototype that merges the argument window into the Window the method is called on', () => {
+  it('has a joinWindows method on its prototype that merges the argument window into the Window object the method is called on', () => {
     const chromeWindowTwo = new BrowserWindow('Jillian Smith', 'Random', [
       { tabURL: 'youtube.com' },
       { tabURL: 'netflix.com' },
@@ -87,7 +87,8 @@ describe('BrowserWindow', () => {
 
     firefoxWindow.closeTab(0);
     // Many browsers have a feature when all tabs are closed, your "default" tab opens
-    // If you close all the tabs, the default tab will re-open
+    // If you close all the tabs, the default tab will re-open, this is why the "defaultHomepage.com" tab
+    // is the only tab open.
     expect(firefoxWindow.tabs).toEqual([{ tabURL: 'defaultHomePage.com' }]);
   });
 
