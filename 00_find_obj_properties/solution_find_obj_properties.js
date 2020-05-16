160924db865b4b5260a8232d6e8189b42fc3b26c
Dean Gladish <deangladish@Deans-MacBook-Air.local>
2020-05-15 23:21:16 -0500

    here are my solutions, I will be glad to hear feedback, thanks in advance! :)

--- 00_find_obj_properties/solution_find_obj_properties.js
+++ 00_find_obj_properties/solution_find_obj_properties.js
@@ -1,2 +1,35 @@
 /* eslint-disable no-unused-vars */
 
+/* Basically what the Object.keys() function 
+does, is that it generates an array consisting 
+of all of the keys, excluding the values to which 
+they correspond within the object */
+const findObjKeys = (obj) => {
+    let result = [];
+    let keys = Object.keys(obj); 
+    // extract the key from each key:value pair 
+    for (let i = 0; i < keys.length; i++) {
+        result.push(keys[i]);
+    }
+    /* The object's keys have to be comma delimited 
+    so that's why I'm using the .join() method for
+    arrays. */
+    result = result.join(', ');
+    return result;
+}
+
+const findObjPropsHasOwn = (obj) => {
+    let result = [];
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            /* if the property/key belongs to the 
+            object obj and isn't inherited then 
+            we are allowed to push it to the result, 
+            per the specifications. */
+            result.push(key);
+        }
+    }
+    result = result.join(', ');
+    return result;
+}
+
--- 03_alphabet_soup/solution_alphabet_soup.js
+++ 03_alphabet_soup/solution_alphabet_soup.js
@@ -1,2 +1,41 @@
 /* eslint-disable no-unused-vars */
-
+/* I'm going to go with my initial idea, 
+which is to iterate through the phrase, 
+removing the current character from 
+characterBank. */
+const soup = (phrase, characterBank) => {
+    let boolean = true;
+    let phraseArray = phrase.split('');
+    let characterBankArray = characterBank.split('');
+    for (let i = 0; i < phraseArray.length; i++) {
+        let currentChar = phraseArray[i];
+        /* if it's not in the character bank, then 
+        there simply aren't enough characters available 
+        and the phrase cannot be created from it. */
+        if (!characterBankArray.includes(currentChar)) {
+            boolean = false;
+        }
+        /* For each character in the phrase, 
+        iterate through the character bank and 
+        as soon as you encounter the character, 
+        remove it and then break. */
+        for (let j = 0; j < characterBankArray.length; j++) {
+            /* This part is important; if the current character 
+            matches the value at j in the character bank array, 
+            then remove that value.  Originally I had said 
+            characterBankArray[i], but the debugger showed me 
+            that since i remains at 0 it had to be j. */
+            if (characterBankArray[j] === currentChar) {
+                characterBankArray.splice(j, 1);
+                break; 
+                /* Only remove the first occurrence
+                of that character */
+            }
+        }
+    }
+    return boolean;
+}
+/* The way I've written it ensures that 
+the character bank has to have an instance of 
+each character in phraseArray, no matter what 
+it is. */
